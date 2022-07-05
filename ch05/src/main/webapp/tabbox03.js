484e45c599fde483e2130d119b7ab8eaa3d6b218
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-05 16:59:00 +0900

    tabbox jQuery로 변경

---
+++ ch05/src/main/webapp/tabbox03.js
@@ -0,0 +1,14 @@
+// jQuery로 작성하기
+
+$(function() {
+    var $obj = $('li');
+    $obj.click(function(){
+        for (let i = 0; i < $obj.length; i++) {
+            if($($obj[i]).hasClass("selected")){
+                $($obj[i]).removeClass("selected");
+            }
+        }        
+        $(this).hasClass("selected") ? $(this).removeClass("selected") :  $(this).addClass("selected") ;        
+    })
+    
+})
