d0d53bbec2dc2ae8e445864d3f294f269400ca29
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-06 15:30:07 +0900

    AJAX #2

--- ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.douzone.ch08.vo.GuestBookVo;
+
 @Controller
 @RequestMapping("/api")
 public class ApiController {
@@ -11,5 +13,23 @@ public class ApiController {
 	@RequestMapping("/text")
 	public String text() {
 		return "Text 데이터";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "<h1>Ajax 연습</h1><p>html 데이터</p>";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/json")
+	public Object json() {
+		GuestBookVo vo = new GuestBookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("둘리가 쓴 글");
+		vo.setRegdate("2022-07-06");
+		
+		return vo;
 	}
 }
